f05255b9adde9d9c0e93390bf3d36e09a8b9f78d
20521811quachvinhquang <hynoras1@gmail.com>
2023-12-01 15:06:05 +0700

    Update Login and Register UI

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -5,35 +5,63 @@ import { Button, Checkbox, Form, Input } from 'antd';
 import { useState } from 'react';
 const cx = classNames.bind(styles);
 
-export default function RegisterPage() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+export default function RegisterPage({ onLoginClick }) {
 
-    async function register(ev) {
-      ev.preventDefault();
-      const response = await fetch('http://localhost:3000/register', {
-        method: 'POST',
-        body: JSON.stringify({username,password}),
-        headers: {'Content-Type':'application/json'},
-      });
-      if (response.status === 200) {
-        alert('Registration successful');
-      } else {
-        alert('Registration failed');
-      }
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [showLogin, setShowLogin] = useState(false);
+
+
+  async function register(ev) {
+    ev.preventDefault();
+    const response = await fetch('http://localhost:3000/register', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.status === 200) {
+      alert('Registration successful');
+    } else {
+      alert('Registration failed');
     }
-    return (
-      <form className={cx('register')} onSubmit={register}>
-        <h1>Register</h1>
-        <input type="text"
-               placeholder="username"
-               value={username}
-               onChange={ev => setUsername(ev.target.value)}/>
-        <input type="password"
-               placeholder="password"
-               value={password}
-               onChange={ev => setPassword(ev.target.value)}/>
-        <button>Register</button>
-      </form>
-    );
   }
+
+  return (
+    <form 
+      className={cx('register')} 
+      onSubmit={register}
+    >
+      <h1
+        className={cx('register-title')}
+      >
+        Register
+      </h1>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(ev) => setUsername(ev.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(ev) => setPassword(ev.target.value)}
+      />
+      <button>
+        Register
+        </button>
+      <p>
+        Already have an account?
+        <span>
+          <a 
+            className={cx('login-redirect')} 
+            onClick={onLoginClick}
+          >
+            Login
+          </a>
+        </span>
+      </p>
+    </form>
+  );
+}
