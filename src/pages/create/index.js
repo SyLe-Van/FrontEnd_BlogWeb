a21b60fefb6f1f25c74fd2d8a7e17d0c2847ea1f
20521811quachvinhquang <hynoras1@gmail.com>
2023-12-07 13:05:03 +0700

    Update index.js

--- src/pages/create/index.js
+++ src/pages/create/index.js
@@ -39,7 +39,7 @@ export default function CreatePost() {
     data.append('file', files[0]);
    
     ev.preventDefault();
-    const response = await fetch('http://localhost:3000/post', {
+    const response = await fetch('http://localhost:3000/post/createPost', {
       method: 'POST',
       body: data,
       credentials: 'include',
